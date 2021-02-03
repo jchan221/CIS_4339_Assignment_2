11d2a09e3c36a9eef782db45c7d061be4346dc37
Joshua Chan <66494146+jchan221@users.noreply.github.com>
2021-02-02 19:29:00 -0600

    first commit

---
+++ Assignment_2.js
@@ -0,0 +1,9 @@
+//Joshua Chan CIS 4339 1588459
+var courses = ["CIS4339","CIS4338", "HRD3351", "CIS3320", "CIS4375"]
+var i
+while (i = 0, i < courses.length, i++){
+    console.log(courses[i])}
+
+var trapezoid = [1,2,3,4,5,6,7,8,9,10]
+var index
+
