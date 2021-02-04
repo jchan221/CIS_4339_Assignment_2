2bccb63425135772622b71f00686a234af99354e
Joshua Chan <66494146+jchan221@users.noreply.github.com>
2021-02-03 19:31:24 -0600

    added height capture

--- Assignment_2.js
+++ Assignment_2.js
@@ -1,9 +1,9 @@
 //Joshua Chan CIS 4339 1588459
-//let courses = ["CIS4339","CIS4338", "HRD3351", "CIS3320", "CIS4375"];
-//let i;
-//for (i = 0; i < courses.length; i++){
-//   console.log(courses[i]);
-//}
+let courses = ["CIS4339","CIS4338", "HRD3351", "CIS3320", "CIS4375"];
+let i;
+for (i = 0; i < courses.length; i++){
+   console.log(courses[i]);
+}
 let count;
 let newTrapArea = [];
 for (count = 1; count < 11; count++)
@@ -24,10 +24,12 @@ for (count = 1; count < 11; count++)
     ];
     newTrapArea.push.apply(newTrapArea, trapArea);
 };
-//console.log(newTrapArea);
+console.log(newTrapArea);
 
 let onlyBase20 = newTrapArea.filter(function (e) {
     return e.trapBaseA === 20;
 });
 console.log(onlyBase20);
 
+let allHeight = newTrapArea.map(a => a.trapHeight);
+console.log(allHeight);
